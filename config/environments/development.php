e60f17523ed06a127c82f3bfd16fdcc99aef8b1a
randallhedglin <randallhedglin@yahoo.com>
2022-10-13 12:23:10 -0400

    WIP commit

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,13 +8,17 @@ use function Env\env;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
-Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+//Config::define('WP_MEMORY_LIMIT', '256M');
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
 
 ini_set('display_errors', '1');
+//ini_set('upload_max_filesize', '256M');
+//ini_set('post_max_size', '256M');
+//ini_set('memory_limit', '512M');
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
